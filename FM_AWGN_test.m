e2d73b7453c416ed18e97d65dfd8534911e79c62
Jacques Visser <jcqvisser@gmail.com>
2015-01-15 16:16:46 +0200

    Rewrote test on adding AWGN to FM
    Noticed the chi squared (?) distribution of sigma_dp, added histogram of
    sigma_dp

--- FM_AWGN_test.m
+++ FM_AWGN_test.m
@@ -1,34 +1,45 @@
 %%
-%Import audio, take clip, upsample and do AMR
+%Import audio
 [audio, f_audio] = audioread('02.mp3');
+fs = 256e3;
+Ns = 4096*2; %desired no of samples
 
 samp_ratio = fs/f_audio;
 N_s_audio = ceil(Ns / samp_ratio); %number of audio samples that would translate to desired numer of samples if upsampled
 t_clip = 0:1/f_audio:N_s_audio/f_audio; %timebase of the audio clip, prior to upsampling
 
-f_c = 80e3;
-clips = 1000;
 
-P_fm = zeros(1,clips);
-sigma_dp_fm = zeros(1,clips);
-gamma_max_fm = zeros(1,clips);
-for i = 1:clips
+ts = 0:1/fs:(Ns-1)/fs; %timebase at desired sampling rate
+
+%%
+% filter
+order = 50;
+fc = 80e3 - 3.16;
+width = 10e3;
+fcol = fc - width;
+fcoh = fc + width;
+filter = fir1(order, [2*fcol/fs, 2*fcoh/fs]);
+
+%%
+% Generation and feature extraction
+f_c = 80e3;
+j_max = 10000;
+
+snr = zeros(1,j_max);
+sigma_dp = zeros(1,j_max);
+gamma_max = zeros(1,j_max);
+
+for j = 1:j_max
     si = round((length(audio)-2*N_s_audio)*rand()) + N_s_audio;
     sf = si + N_s_audio;
-    audio_clip = audio(si:sf); %take random clip from audio
+    audio_clip = audio(si:sf);
     
-    %upsample the clip using cubic interpolation.
     x = interp1(t_clip, audio_clip, ts, 'cubic'); 
     
-    s_fm = cos(2*pi*f_c.*ts + cumsum(x)) + clips/i*normrnd(1,1,1,Ns);
-    s_fm = conv(s_fm, filter, 'same');
+    s = cos(2*pi*f_c.*ts + cumsum(x)) + 0.5*normrnd(1,1,1,Ns);
+    s = conv(s, filter, 'same');
     
-    [P_fm(i), sigma_dp_fm(i), gamma_max_fm(i)] = AMRA(s_fm, ts, fs);
+    [~, sigma_dp(j), gamma_max(j)] = AMRA(s, ts, fs);
 end
-
-figure;
-dot_size = 10;
-scatter3(gamma_max_am, (1:1:clips)/clips , sigma_dp_am, dot_size, [1,0,0], 'fill');
-xlabel('gamma_ma}');
-ylabel('signal to noise raio');
-zlabel('sigma_dp');
+%%
+hist(sigma_dp,100)
